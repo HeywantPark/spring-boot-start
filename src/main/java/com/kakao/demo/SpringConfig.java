4962180f6587d6fe4b57160c17f13de442a2354b
Park Hyewon <db1648@naver.com>
2024-08-18 14:19:52 +0900

    240818 jdbc로 스프링 db접근

--- src/main/java/com/kakao/demo/SpringConfig.java
+++ src/main/java/com/kakao/demo/SpringConfig.java
@@ -1,19 +1,28 @@
 package com.kakao.demo;
 
+import com.kakao.demo.repository.JdbcMemberRepo;
 import com.kakao.demo.repository.MemberRepo;
 import com.kakao.demo.repository.MemoryMemberRepo;
 import com.kakao.demo.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepo());
     }
     @Bean
     public MemberRepo memberRepo() {
-        return new MemoryMemberRepo();
+//        return new MemoryMemberRepo();
+        return new JdbcMemberRepo(dataSource);
     }
 }
